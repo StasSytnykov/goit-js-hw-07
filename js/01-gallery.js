f80d25bdba039a8a9ae2d96df851ce3417ec7a5a
Stas Sytnykov <stas82kak05@gmail.com>
2022-01-19 16:55:07 +0200

    connected the basiclightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -35,3 +35,9 @@ function onGalleryItemClick(event) {
   }
   console.log(event.target.dataset.source);
 }
+
+const instance = basicLightbox.create(`
+	<h1>Dynamic Content</h1>
+	<p>You can set the content of the lightbox with JS.</p>
+`);
+console.log(instance);
