9f96a5449ff491ff8aa1d46dcc04741d00de997e
Stas Sytnykov <stas82kak05@gmail.com>
2022-01-19 15:08:07 +0200

    add markup and addEventListenerOnLink

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,35 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
-
 const gallery = document.querySelector(".gallery");
 
+const makeGalleryMarkup = ({ preview, original, description }) => {
+  return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+};
+
+const makeGallery = galleryItems.map(makeGalleryMarkup).join("");
+
+gallery.innerHTML += makeGallery;
+
+gallery.addEventListener("click", onGalleryItemClick);
+
+const link = document.querySelectorAll(".gallery__link");
+
+function addEventListenerOnLink() {
+  link.forEach((element) =>
+    element.addEventListener("click", (event) => event.preventDefault())
+  );
+}
+addEventListenerOnLink();
+
+function onGalleryItemClick(event) {
+  console.log(event.target);
+}
