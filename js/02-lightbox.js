3f41d9580880cac91b22d4c0f0d53f446accec2b
Stas Sytnykov <stas82kak05@gmail.com>
2022-01-20 16:53:39 +0200

    completed task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,13 +1,20 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const gallery = document.querySelector(".gallery");
+const galleryList = document.querySelector(".gallery");
 
 const makeGalleryMarkup = ({ preview, original, description }) => {
   return `<a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
 </a>`;
 };
 
 const makeGallery = galleryItems.map(makeGalleryMarkup).join("");
 
-gallery.innerHTML += makeGallery;
+galleryList.innerHTML += makeGallery;
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
+gallery.on("show.simplelightbox", function () {
+  // do something…
+});
