e854fdad80c8866e85d65986e22b685556ffdcf5
Stas Sytnykov <stas82kak05@gmail.com>
2022-01-20 15:43:47 +0200

    setup and connect library SimpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,13 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const makeGalleryMarkup = ({ preview, original, description }) => {
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+};
+
+const makeGallery = galleryItems.map(makeGalleryMarkup).join("");
+
+gallery.innerHTML += makeGallery;
